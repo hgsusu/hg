e6783076474744255ee8eb2cb6442a01611d673b
hgsusu <765261521@qq.com>
2017-10-16 18:58:44 +0800

    loginhg

---
+++ app/src/main/java/com/example/administrator/hg/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.hg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/10/16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
