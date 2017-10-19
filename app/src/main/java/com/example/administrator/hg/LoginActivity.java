9cddcd35c46c73347def1dbbb0b823a01dfffd3c
hgsusu <765261521@qq.com>
2017-10-19 11:04:46 +0800

    zhuye

--- app/src/main/java/com/example/administrator/hg/LoginActivity.java
+++ app/src/main/java/com/example/administrator/hg/LoginActivity.java
@@ -1,17 +1,32 @@
 package com.example.administrator.hg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
 /**
  * Created by Administrator on 2017/10/16.
  */
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Button dl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        dl = (Button) findViewById(R.id.dl);
+        dl.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, ZhuyeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/administrator/hg/ZhuyeActivity.java
@@ -0,0 +1,18 @@
+package com.example.administrator.hg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/10/19.
+ */
+
+public class ZhuyeActivity  extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zhuye);
+    }
+}
